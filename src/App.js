190b54766bafd855e8d9f3830e313bd46e3c81b5
Akhileshreddy-py <reddyakhilesh94@gmail.com>
2021-06-05 10:18:40 +0530

    3 files modified

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import "./style.css";
 export default function App(props) {
   return (
     <div className="akhi">
-      <h1>name:{props.name}</h1>
-      <h4>age:{props.age}</h4>
+      <h1>Name:{props.name}</h1>
+      <h4>Age:{props.age}</h4>
     </div>
   );
 }
